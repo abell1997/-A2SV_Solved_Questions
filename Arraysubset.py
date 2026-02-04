f555c9f058216767e4ba62cbdedf7f1eac58fbd0
abell1997 <abeltamerat1997@gmail.com>
2026-02-04 11:45:12 +0100

    Added file

---
+++ Arraysubset.py
@@ -0,0 +1,12 @@
+#Geeks for Geeks
+
+
+class Solution:
+    def isSubset(self, a, b):
+        set_a = set(a)
+        set_b = set(b)
+        
+        for element in set_b:
+            if element not in set_a:
+                return False
+        return True
