58d4219936cf73643b66afaedab7bc78aa98cae1
abell1997 <abeltamerat1997@gmail.com>
2026-03-21 11:03:14 +0100

    Added file

--- B_Most_socially_distanced_subsequence.py
+++ B_Most_socially_distanced_subsequence.py
@@ -3,15 +3,15 @@ input = sys.stdin.readline
 
 t = int(input())
 
-for _ in range(t):
+for x in range(t):
     n = int(input())
     a = list(map(int, input().split()))
     
-    rel = [a[0]]
+    res = [a[0]]
     for i in range(1, n - 1):
         if (a[i] > a[i-1] and a[i] > a[i+1]) or (a[i] < a[i-1] and a[i] < a[i+1]):
-            rel.append(a[i])
-    rel.append(a[-1])
+            res.append(a[i])
+    res.append(a[-1])
     
-    print(len(rel))
-    print(*rel)
+    print(len(res))
+    print(*res)
